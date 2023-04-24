3dc2ed42fcbadcaa4cd0d697432de62c62217295
AJAY KUMAR <kumarajay.rs3322@gmail.com>
2023-04-24 11:49:43 +0530

    Initial commit

---
+++ lib/Screen/Profile_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class MyProfile extends StatefulWidget {
+  const MyProfile({Key? key}) : super(key: key);
+
+  @override
+  State<MyProfile> createState() => _MyProfileState();
+}
+class _MyProfileState extends State<MyProfile> {
+  @override
+  Widget build(BuildContext context) {
+   var _size= MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 58, 84),
+        title: const Text('Profile',style: TextStyle(color: Colors.black),),
+        leading:IconButton(onPressed: (){Navigator.of(context).pop();}, icon: const Icon(Icons.arrow_back_ios,color:Colors.black,)),
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(Icons.logout,color:Colors.black,)),
+        ],
+      ),
+      body: Column(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 40,),
+              SizedBox(
+                width:_size.width ,
+                child: const CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 52,
+                ),
+              ),
+              const Text('Alexander',style: TextStyle(color: Colors.black,fontSize: 34),),
+              const Text('Utrakhand',style: TextStyle(color: Colors.black,fontSize: 24),),
+              const Text('Ajay',style: TextStyle(color: Colors.black,fontSize: 14),),
+              SizedBox(height: 15,),
+              Divider(thickness: 2,height: 2,)
+            ],
+          ),
+
+          SizedBox(height: 20,),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Wrap(
+                spacing: 20,
+                runSpacing: 20,
+                children: [
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.red,
+                    ),
+                  ),
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.red,
+                    ),
+                  ),
+
+                  SizedBox.square(
+                    dimension: 130,
+                    child: Card(
+                      color: Colors.red,
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'Screen/intro_screen.dart';
+import 'Utils/utils.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Fem Care',
+          // You can use the library anywhere in the app even in theme
+          theme:ThemeData(
+                  primarySwatch: Colors.blue,
+                  inputDecorationTheme: InputDecorationTheme(
+                    filled: true,
+                    fillColor: Colors.white38,
+                    border: InputBorder.none,
+                    hintStyle: TextStyle(color: Colors.white),
+                    contentPadding: EdgeInsets.symmetric(
+                        vertical: defpaultPadding * 1.2, horizontal: defpaultPadding),
+                  ),
+                ),
+          // ThemeData(
+          //   primarySwatch: Colors.blue,
+          //   textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          //   inputDecorationTheme: InputDecorationTheme(
+          //     filled: true,
+          //     fillColor: Colors.white38,
+          //     border: OutlineInputBorder(
+          //       borderRadius: BorderRadius.circular(8),
+          //       borderSide: const BorderSide(color: Color.fromRGBO(255, 255, 51, 90))
+          //     ),
+          //     focusedBorder: OutlineInputBorder(
+          //         borderRadius: BorderRadius.circular(8),
+          //         borderSide: const BorderSide(color: Color.fromRGBO(255, 255, 51, 90))
+          //     ) ,
+          //     enabledBorder: OutlineInputBorder(
+          //         borderRadius: BorderRadius.circular(8),
+          //         borderSide: const BorderSide(color: Color.fromRGBO(255, 255, 51, 90))
+          //     ) ,
+          //     hintStyle: TextStyle(color: Colors.white),
+          //     contentPadding: EdgeInsets.symmetric(
+          //         vertical: defpaultPadding * 1.2, horizontal: defpaultPadding),
+          //   ),
+          // ),
+          home: const IntroScreen(),
+        );
+      },
+
+    );
+  }
+}
+
+
